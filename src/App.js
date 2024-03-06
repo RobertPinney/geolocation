6e548288ad7c9b535c546758ee5db385f97ec1a7
Robert <Robert.pinney@ecit.no>
2024-03-06 10:24:49 +0100

    first and likely last commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useGeolocation } from "./usegeolocation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [countClicks, setCountClicks] = useState(0);
+
+	const {
+		isLoading,
+		position: { lat, lng },
+		error,
+		getPosition,
+	} = useGeolocation();
+
+	function handleClick() {
+		setCountClicks((count) => count + 1);
+		getPosition();
+	}
+
+	return (
+		<div>
+			<button onClick={handleClick} disabled={isLoading}>
+				Get my position
+			</button>
+
+			{isLoading && <p>Loading position...</p>}
+			{error && <p>{error}</p>}
+			{!isLoading && !error && lat && lng && (
+				<p>
+					Your GPS position:{" "}
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+					>
+						{lat}, {lng}
+					</a>
+				</p>
+			)}
+
+			<p>You requested position {countClicks} times</p>
+		</div>
+	);
 }
-
-export default App;
